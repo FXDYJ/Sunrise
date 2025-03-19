14bd8134237cfaf4f3ccd9eb4543818b6b07b467
R2kip <56793105+R2kip@users.noreply.github.com>
2025-03-19 19:58:59 +0300

    wip

--- Sunrise/Features/CustomVisibility/FpcVisibilityControllerPatch.cs
+++ Sunrise/Features/CustomVisibility/FpcVisibilityControllerPatch.cs
@@ -3,8 +3,10 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection.Emit;
 using Exiled.API.Enums;
+using Exiled.API.Extensions;
 using HarmonyLib;
 using JetBrains.Annotations;
+using MapEditorReborn.API.Extensions;
 using MapGeneration;
 using PlayerRoles.FirstPersonControl;
 using PlayerRoles.Visibility;
@@ -24,14 +26,16 @@ namespace Sunrise.Features.CustomVisibility;
 [HarmonyPatch(typeof(FpcVisibilityController), nameof(FpcVisibilityController.GetActiveFlags))] [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public static class VisibilityPatch
 {
+    private static Dictionary<Vector3Int, HashSet<Vector3Int>> _roomMap = new();
+    
     static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
     {
         List<CodeInstruction> newInstructions = instructions.ToList();
 
         newInstructions.InsertRange(newInstructions.Count - 1,
         [
-            new(OpCodes.Ldloc_1), // currentRole1
-            new(OpCodes.Ldloc, 3), // position2
+            new(OpCodes.Ldloc_1), // currentRole1 (observer)
+            new(OpCodes.Ldloc, 3), // position2 (owner)
             new(OpCodes.Call, Method(typeof(VisibilityPatch), nameof(AddCustomVisibility))),
         ]);
 
@@ -40,7 +44,6 @@ public static class VisibilityPatch
 
     public static bool Enabled = false;
     public static float SmallerRoomGridSize = 9;
-    static int counter = 0;
 
     /// <summary>
     ///     This method limits visibility diagonally when players are inside the facility.
@@ -49,25 +52,64 @@ public static class VisibilityPatch
     static InvisibilityFlags AddCustomVisibility(InvisibilityFlags flags, IFpcRole role1, Vector3 position2)
     {
         // players are out of range
-        if (!Enabled || (flags & InvisibilityFlags.OutOfRange) != 0)
+        if (!Enabled || flags.HasFlagFast(InvisibilityFlags.OutOfRange))
             return flags;
 
         Vector3 position1 = role1.FpcModule.Position;
-        Vector3Int cords1 = RoomIdUtils.PositionToCoords(position1);
-        Vector3Int cords2 = RoomIdUtils.PositionToCoords(position2);
+        Vector3Int coords1 = RoomIdUtils.PositionToCoords(position1); // observer
+        Vector3Int coords2 = RoomIdUtils.PositionToCoords(position2); // owner
 
-        // If rooms are diagonal to each other
-        if (cords1.x != cords2.x && cords1.z != cords2.z)
-        {
-            if (counter++ % 30 == 0)
-                Log.Warn($"{role1.FpcModule?.Hub?.nicknameSync?.MyNick} cant see - Position1: {position1}, Position2: {position2}, Cords1: {cords1}, Cords2: {cords2}");
-
+        if (!_roomMap.TryGetValue(coords1, out var roomMap))
+            _roomMap[coords1] = roomMap = GetSeeingCoords(Room.Get(position1));
+        
+        if (!roomMap.Contains(coords2))
             return flags | InvisibilityFlags.OutOfRange;
-        }
-
-        if (counter++ % 30 == 0)
-            Log.Warn($"{role1.FpcModule?.Hub?.nicknameSync?.MyNick} - Position1: {position1}, Position2: {position2}, Cords1: {cords1}, Cords2: {cords2}");
 
         return flags;
+    }
+    
+    private static readonly Vector3Int[] Directions =
+    [
+        Vector3Int.forward,
+        Vector3Int.right,
+        Vector3Int.back,
+        Vector3Int.left
+    ];
+
+    public static HashSet<Vector3Int> GetSeeingCoords(Room startRoom)
+    {
+        Vector3Int roomCoords = RoomIdUtils.PositionToCoords(startRoom.Position);
+        HashSet<Vector3Int> seeing = [roomCoords];
+
+        foreach (Vector3Int direction in Directions)
+        {
+            Room currentRoom = startRoom;
+            Vector3Int currentCoords = roomCoords;
+            
+            while (true)
+            {
+                Room nextRoom = null;
+                
+                foreach (Room room in currentRoom.NearestRooms)
+                {
+                    Vector3Int coords = RoomIdUtils.PositionToCoords(room.Position);
+
+                    if (coords - currentCoords == direction)
+                    {
+                        seeing.Add(coords);
+                        nextRoom = room;
+                        currentCoords = coords;
+                        break;
+                    }
+                }
+                
+                if (nextRoom == null)
+                    break;
+                
+                currentRoom = nextRoom;
+            }
+        }
+        
+        return seeing;
     }
 }
