7ebc84c9e4b48eb3ad275a950a4b691a6c44cbde
Banalny-Banan <elisey1324@gmail.com>
2025-03-22 08:30:44 +0200

    config descriptions

--- Sunrise/EntryPoint/Config.cs
+++ Sunrise/EntryPoint/Config.cs
@@ -10,28 +10,22 @@ public class Config : IConfig
     public bool IsEnabled { get; set; } = true;
     public bool Debug { get; set; } = false;
 
-    [Description("Enables some visual debugging features")]
+    [Description("Enables some visual debugging features.")]
     public bool DebugPrimitives { get; set; } = false;
 
-    [Description(
-        """
-        The maximum latency for which the server has to account for.
-        Higher values give more authority to clients, lower values may decrease gameplay quality for players with higher latency.
-        """
-    )]
+    [Description("The maximum latency for which the server has to account. Higher values give more authority to clients, lower values may decrease gameplay quality for players with higher latency.")]
     public float AccountedLatencySeconds { get; set; } = 0.3f;
 
-    [Category("Toggle features separately")]
-    [Description("Limits the information sent to players. Performance impact not yet measured.")]
+    [Description("Toggle features separately:\nSignificantly reduces wallhack usefulness. Performance impact not yet measured.")]
     public bool AntiWallhack { get; set; } = true;
 
-    [Description("Enables the serverside door damage feature. Preformance impact negligible.")]
+    [Description("Prevents picking up items through walls. Performance impact negligible.")]
     public bool PickupValidation { get; set; } = true;
 
-    [Description("Enables the serverside backtracking feature. Improves performance.")]
+    [Description("Prevents silent-aim and spinbots. Actually improves performance.")]
     public bool ServersideBacktrack { get; set; } = true;
 
-    [Description("Enables the serverside tesla damage feature. Preformance impact negligible.")]
+    [Description("Prevents cheaters from ignoring tesla damage. Performance impact negligible.")]
     public bool ServersideTeslaDamage { get; set; } = true;
 
     #region Singleton
