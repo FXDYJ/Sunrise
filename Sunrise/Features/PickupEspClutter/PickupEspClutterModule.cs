a38a1701112bd7e384dd5a4b21caa25885bd4b9c
Banalny-Banan <elisey1324@gmail.com>
2025-03-23 19:33:33 +0200

    upd

--- Sunrise/Features/PickupEspClutter/PickupEspClutterModule.cs
+++ Sunrise/Features/PickupEspClutter/PickupEspClutterModule.cs
@@ -2,7 +2,6 @@ using System;
 using Exiled.Events.EventArgs.Map;
 using Exiled.Events.EventArgs.Server;
 using InventorySystem.Items.Usables;
-using MEC;
 
 namespace Sunrise.Features.PickupEspClutter;
 
@@ -28,57 +27,5 @@ public class PickupEspClutterModule : PluginModule
     static void OnRoundEnded(RoundEndedEventArgs ev)
     {
         PhantomItemSpawner.Stop();
-    }
-}
-
-public static class PhantomItemSpawner
-{
-    static readonly List<ItemType> PhantomItemTypes =
-    [
-        ItemType.SCP500,
-    ];
-
-    static CoroutineHandle coroutineHandle;
-
-    public static void Start()
-    {
-        Timing.KillCoroutines(coroutineHandle);
-        coroutineHandle = Timing.RunCoroutine(PhantomItemSpawnerCoroutine());
-    }
-
-    public static void Stop()
-    {
-        Timing.KillCoroutines(coroutineHandle);
-    }
-
-    static IEnumerator<float> PhantomItemSpawnerCoroutine()
-    {
-        List<Room> rooms = new(Room.List);
-        const int MaxCount = 300;
-        int intialCount = MaxCount;
-
-        while (true)
-        {
-            rooms.ShuffleList();
-
-            foreach (Room room in rooms)
-            {
-                const float RandomOffset = 5f;
-                Vector3 position = room.Position + (Random.insideUnitSphere * RandomOffset) with { y = 0.5f };
-                ItemType itemType = PhantomItemTypes.RandomItem();
-
-                PhantomItem.Create(itemType, position);
-
-                if (intialCount-- > 0)
-                    continue;
-
-                while (PhantomItem.List.Count >= MaxCount)
-                    yield return Timing.WaitForSeconds(1f);
-
-                yield return Timing.WaitForSeconds(1f + Random.value);
-            }
-
-            yield return Timing.WaitForSeconds(1f + Random.value);
-        }
     }
 }
